f405d919b39368d02343b8f585c80c8f57c45d95
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2026-03-20 19:49:20 -0500

    Added build & clean scripts.

---
+++ clean.bat
@@ -0,0 +1,32 @@
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::Clean CMZServerHost Via MSBuild                                ::
+::Gihub https://github.com/RussDev7/CMZDedicatedServer           ::
+::Developed, Maintained, And Sponsored By RussDev7, unknowghost0 ::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@ECHO OFF
+
+Rem | Check If Clean Is Necessary
+if not exist *.zip (
+    echo ERROR: You need to run build.bat first!
+	echo(
+	pause
+	goto :EOF
+)
+
+Rem | Clean Up Files
+FOR /d %%a IN ("%~dp0\*") DO IF /i NOT "%%~nxa"=="release" RD /S /Q "%%a"
+FOR %%a IN ("%~dp0\*") DO IF /i NOT "%%~nxa"=="clean.bat" IF /i NOT "%%~xa"==".zip" DEL "%%a"
+
+Rem | Move Files Out Of Release
+FOR /d %%g IN ("%~dp0release\*") do set "correctDir=%%~dpnxg"
+robocopy "%correctDir%" "%~dp0\" /E /NJH /NJS /NDL
+
+Rem | Remove Temp Files
+rmdir /s /q "release"
+
+Rem | Operation Complete
+echo(
+pause
+
+Rem | Self Terminate
+del "%~f0"
